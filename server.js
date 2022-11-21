6f5900f69a51be2630b108144e6505599ffbd710
Elena <elenabeylat@gmail.com>
2022-11-21 12:14:19 +0100

    Ajout de jeux à la base de données

--- server.js
+++ server.js
@@ -10,16 +10,50 @@ mongoose.connect("mongodb://localhost:27017/test").then((e)=>{
 })
 
 // Create models
-const Game = mongoose.model("Game", {Name: String, Price: Number, Rating: Number, Description: String, Duration: Number, MinPlayer: Number, MaxPlayer: Number, Category: String, Editor: String, Year: Number});
+// const Game = mongoose.model("Game", {Name: String, Price: Number, Rating: Number, Description: String, Duration: Number, MinPlayer: Number, MaxPlayer: Number, Category: String, Editor: String, Year: Number});
 
-const gameToSave = new Game({Name: "Monopoly", Price: 20, Rating: 4, Description: "Jeu de société", Duration: 120, MinPlayer: 2, MaxPlayer: 4, Category: "Société", Editor: "Hasbro", Year: 1935});
+const Schema = mongoose.Schema;
 
-gameToSave.save().then((e)=>{
-    console.log("Game saved");
+const gameSchema = new Schema({
+    Name: String,
+    Price: Number,
+    Rating: Number,
+    Description: String,
+    Duration: Number,
+    MinPlayer: Number,
+    MaxPlayer: Number,
+    Category: String,
+    Editor: String,
+    Year: Number
+});
+
+const Games = mongoose.model("Games", gameSchema);
+
+// Create a new game
+var dataGames = [
+    {Name: "Azul", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Plan B Games", Year: 2017},
+    {Name: "7 Wonders", Price: 30, Rating: 8, Description: "Un jeu de stratégie", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2010},
+    {Name: "Monopoly", Price: 30, Rating: 8, Description: "Un jeu de plateauu et d'investissement", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Hasbro", Year: 1935},
+    {Name: "Catan", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 1995},
+    {Name: "Puerto Rico", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2002},
+    {Name: "Ticket to Ride", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Days of Wonder", Year: 2004},
+    {Name: "Terraforming Mars", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2016},
+    {Name: "Pandemic", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2008},
+    {Name: "Codenames", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2015},
+    {Name: "Splendor", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2014},
+    {Name: "7 Wonders Duel", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2015},
+    {Name: "Dominion", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2008},
+    {Name: "Kingdomino", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2016},
+    {Name: "Colt Express", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2015},
+    {Name: "Arkham Horror", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2005},
+    {Name: "Mysterium", Price: 30, Rating: 8, Description: "Un jeu de placement de tuiles", Duration: 30, MinPlayer: 2, MaxPlayer: 4, Category: "Stratégie", Editor: "Repos Production", Year: 2015},
+]
+
+Games.insertMany(dataGames).then((e)=>{
+    console.log("Data inserted")
 }).catch((e) =>{
-    console.log("Error while saving game");
-})
-
+    console.log("Error while inserting data")
+});
 
 // Création du serveur et initialisation
 const app = express()
@@ -27,21 +61,21 @@ app.use(json())
 
 // Récupérer tous les jeux
 app.get("/games", (request, response) => {
-    Game.find().then((games) => {
+    Games.find().then((games) => {
         response.send(games)
     })
 })
 
 // Récupérer un jeu
 app.get("/games/:id", (request, response) => {
-    Game.findById(request.params.id).then((game) => {
+    Games.findById(request.params.id).then((game) => {
         response.send(game)
     })
 })
 
 // Ajouter un jeu
 app.post("/games", (request, response) => {
-    const game = new Game(request.body)
+    const game = new Games(request.body)
     game.save().then((game) => {
         response.send(game)
     })
@@ -49,14 +83,14 @@ app.post("/games", (request, response) => {
 
 // Modifier un jeu
 app.put("/games/:id", (request, response) => {
-    Game.findByIdAndUpdate(request.params.id, request.body).then(() => {
+    Games.findByIdAndUpdate(request.params.id, request.body).then(() => {
         response.sendStatus(200)
     })
 })
 
 // Supprimer un jeu
 app.delete("/games/:id", (request, response) => {
-    Game.findByIdAndDelete(request.params.id).then(() => {
+    Games.findByIdAndDelete(request.params.id).then(() => {
         response.sendStatus(200)
     })
 })
